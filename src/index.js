e3f7f5a343e0c47ae09d806f247b19553f599191
Nadya Zhernak <makaronchik28@gmail.com>
2020-10-03 23:54:34 +0300

    feat: add brackets solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    if (str.length % 2 !== 0) {
+        return false;
+    } else {
+        let arr = [...str]; 
+        for (let i = 0; i < arr.length; i++) {
+            for (let j = 0; j < bracketsConfig.length; j++) {
+                let openBracket = bracketsConfig[j][0];
+                let closeBracket = bracketsConfig[j][1];
+                if (arr[i] === openBracket && arr[i + 1] === closeBracket) {
+                    arr.splice(i, 2);
+                    i = -1;
+                } else {
+                    continue;
+                }                
+            }            
+        }  
+        if (arr.length === 0) {
+            return true;
+        } else {
+            return false;
+        }      
+    }
 }
